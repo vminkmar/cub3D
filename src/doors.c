9d51868f9c88640187e473b8c0d0dfb07c1145dd
Michael Graefen <michaelgraefen@gmail.com>
2023-05-16 11:41:50 +0200

    weired floats

--- src/doors.c
+++ src/doors.c
@@ -19,12 +19,15 @@ void cast_door_ray(t_player *player, t_data *data, double angle)
 		printf("%f\n", ray->distance);
 		ray->interception.x = ray->start.x + ray->dir.x * ray->distance;
 		ray->interception.y = ray->start.y + ray->dir.y * ray->distance;
-		printf("%f\n", ray->interception.y);
-		printf("%f\n", ray->interception.x);
-		if(data->map->map[(int)ray->interception.y][(int)ray->interception.x] == CLOSED_DOOR && ray->distance <= 1)
+		int x_index = (int)round(ray->interception.x);
+		int y_index = (int)round(ray->interception.y);
+		printf("%d\n", y_index);
+		printf("%d\n", x_index);
+		printf("MAPDATA: %c", data->map->map[y_index][x_index]);
+		if(data->map->map[y_index][x_index] == CLOSED_DOOR && ray->distance <= 1)
 		{
 			printf("HIT\n");
-			data->map->map[(int)ray->interception.y][(int)ray->interception.x] = OPEN_DOOR;
+			data->map->map[y_index][x_index] = OPEN_DOOR;
 		}
 		/* else if (data->map->map[(int)ray->interception.y][(int)ray->interception.x] == OPEN_DOOR && ray->distance >= 0.1)
 			data->map->map[(int)ray->interception.y][(int)ray->interception.x] = CLOSED_DOOR; */
